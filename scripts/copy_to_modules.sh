8ad60dcf2258746830a512a958aca8481c057801
ste7anste7an <stefan@vmi1504892.contaboserver.net>
2024-06-26 20:31:01 +0200

    scripts added

--- scripts/copy_to_modules.sh
+++ scripts/copy_to_modules.sh
@@ -1,19 +1,20 @@
-export MICROPYTHON=/home/stefan/projects/esp32/micropython
-cp -a ../boards/LMS_ESP32 %MICROPYTHON/ports/eps32/boards
-export LMS_ESP32_MODULES=$MICROPYHTON/ports/esp32/boards/LMS_ESP32/modules
-
-cp ../submodules/PUPRemote/src/lpf2.py $PORTS_ESP32_MODULES
-cp ../submodules/PUPRemote/src/pupremote.py $PORTS_ESP32_MODULES
-cp ../submodules/mpy-robot-tools/mpy_robot_tools/uartremote.py $PORTS_ESP32_MODULES
-cp ../submodules/mpy-robot-tools/mpy_robot_tools/servo.py $PORTS_ESP32_MODULES
-cp ../submodules/mpy-robot-tools/mpy_robot_tools/np_animation.py $PORTS_ESP32_MODULES
-cp ../submodules/mpy-robot-tools/mpy_robot_tools/bt.py $PORTS_ESP32_MODULES
-cp ../submodules/mpy-robot-tools/mpy_robot_tools/ctrl_plus.py $PORTS_ESP32_MODULES
-cp ../submodules/mpy-robot-tools/mpy_robot_tools/ctrl_plus.py $PORTS_ESP32_MODULES
-mkdir  $PORTS_ESP32_MODULES/serialtalk
-cp ../submodules/SerialTalk/serialtalk/usockets.py $PORTS_ESP32_MODULES/serialtalk
-cp ../submodules/SerialTalk/serialtalk/esp32.py $PORTS_ESP32_MODULES/serialtalk
-cp ../submodules/SerialTalk/serialtalk/serialtalk.py $PORTS_ESP32_MODULES/serialtalk
-cp ../submodules/SerialTalk/serialtalk/__init__.py $PORTS_ESP32_MODULES/serialtalk
-cp ../submodules/SerialTalk/serialtalk/auto.py $PORTS_ESP32_MODULES/serialtalk
+export MICROPYTHON=/home/stefan/projects/esp32/test/micropython
+cp -a ../boards/LMS_ESP32 $MICROPYTHON/ports/esp32/boards
+export LMS_ESP32_MODULES=$MICROPYTHON/ports/esp32/boards/LMS_ESP32/modules
+mkdir -p $LMS_ESP32_MODULES
+cp ../micropython_modules/*.py $LMS_ESP32_MODULES
+cp ../submodules/PUPRemote/src/lpf2.py $LMS_ESP32_MODULES
+cp ../submodules/PUPRemote/src/pupremote.py $LMS_ESP32_MODULES
+cp ../submodules/mpy-robot-tools/mpy_robot_tools/uartremote.py $LMS_ESP32_MODULES
+cp ../submodules/mpy-robot-tools/mpy_robot_tools/servo.py $LMS_ESP32_MODULES
+cp ../submodules/mpy-robot-tools/mpy_robot_tools/np_animation.py $LMS_ESP32_MODULES
+cp ../submodules/mpy-robot-tools/mpy_robot_tools/bt.py $LMS_ESP32_MODULES
+cp ../submodules/mpy-robot-tools/mpy_robot_tools/ctrl_plus.py $LMS_ESP32_MODULES
+cp ../submodules/mpy-robot-tools/mpy_robot_tools/ctrl_plus.py $LMS_ESP32_MODULES
+mkdir -p $LMS_ESP32_MODULES/serialtalk
+cp ../submodules/SerialTalk/serialtalk/usockets.py $LMS_ESP32_MODULES/serialtalk
+cp ../submodules/SerialTalk/serialtalk/esp32.py $LMS_ESP32_MODULES/serialtalk
+cp ../submodules/SerialTalk/serialtalk/serialtalk.py $LMS_ESP32_MODULES/serialtalk
+cp ../submodules/SerialTalk/serialtalk/__init__.py $LMS_ESP32_MODULES/serialtalk
+cp ../submodules/SerialTalk/serialtalk/auto.py $LMS_ESP32_MODULES/serialtalk
 
