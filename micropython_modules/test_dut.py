593b297e3a77d1d3974871bdef005a79be4e3af6
Stefan Op de Beek <ste7an@gmail.com>
2024-06-30 17:20:35 +0200

    use uartremote in test firmware

--- micropython_modules/test_dut.py
+++ micropython_modules/test_dut.py
@@ -5,36 +5,18 @@ from time import ticks_ms,ticks_diff
 from binascii import hexlify
 from time import sleep_ms
 import lms_esp32
+from uartremote import *
 
-
-LMS_ESP32_RX_PIN = 18
-LMS_ESP32_TX_PIN = 19
-
-LMS_ESP32_V2_RX_PIN = 8
-LMS_ESP32_V2_TX_PIN = 7
+ur=UartRemote(timeout=1000)
 
 version=lms_esp32.version()
 if version==2:
     pins = [5,22,20,0,32,26,14,13,4,21,19,2,33,27,12,15]
     pin_led=25
-    tx_pin=LMS_ESP32_V2_TX_PIN
-    rx_pin=LMS_ESP32_V2_RX_PIN
 else:
     pins = [5,22,25,2,26,27,32,33,4,21,23,0,12,13,14,15]
     pin_led=25
-    tx_pin=LMS_ESP32_TX_PIN
-    rx_pin=LMS_ESP32_RX_PIN
-baudrate=115200
-uart=UART(1,baudrate=baudrate,rx=rx_pin,tx=tx_pin,timeout=1)
 
-
-def wait_for_ack():
-    start = ticks_ms()
-    ack=False
-    while ticks_diff(ticks_ms(), start) < 1000 and not ack:
-        a=uart.read()
-        ack = a==b'ack'
-    return ack
 
 def write_gpio(pin,val):
     p = Pin(pin, Pin.OUT)
@@ -45,19 +27,12 @@ def test_pin(pin,state=1):
     for p in pins:
         pp = Pin(p, Pin.IN, pull=None)
     write_gpio(pin,state)
-    if state==1:
-        s='H'
-    else:
-        s='L'
-    uart.write(s+"%02d"%pin)
-    ack=wait_for_ack()
-    if not ack:
-        print('time out')
+    print("testpin",pin,state)
+    _=ur.call('test_pin','2b',pin,state)
 
 def write_id():
     id=hexlify(unique_id())
-    uart.write(b"ID="+id)
-    ack=wait_for_ack()
+    ur.call('mac','12s',id)
     
 def test_program():
     print("start test program")
@@ -70,18 +45,16 @@ def test_program():
         sleep_ms(10)
     # set pin_led as IN 
     Pin(pin_led, Pin.IN)
-    uart.write('stop')
+    ur=UartRemote(timeout=3000)
+    ur.call('stop')
     
 def start_test():
-  sleep_ms(200) # wait    
-  start = ticks_ms()
-  uart.read() # empty read buf
-  uart.write('start')
-  ack=False
-  while ticks_diff(ticks_ms(), start) < 1000 and not ack:
-      a=uart.read()
-      ack = a==b'ack'
-  if ack:
+  #sleep_ms(200) # wait
+  ur=UartRemote(timeout=1000)
+  err,_=ur.call('start')
+  if err!='err':
       test_program()
   else:
       print('ready to start coding')
+ 
+
--- micropython_modules/tester.py
+++ micropython_modules/tester.py
@@ -4,7 +4,7 @@ from machine import UART,Pin
 from neopixel import NeoPixel
 from time import sleep_ms
 import lms_esp32
-
+from uartremote import *
 
 LMS_ESP32_RX_PIN = 18
 LMS_ESP32_TX_PIN = 19
@@ -24,8 +24,8 @@ else:
     tx_pin=LMS_ESP32_TX_PIN
     rx_pin=LMS_ESP32_RX_PIN
     
-baudrate=115200
-uart=UART(1,baudrate=baudrate,rx=rx_pin,tx=tx_pin,timeout=1,timeout_char=1)
+ur=UartRemote()
+total_errors=0
 
 def print_pins(state):
     #state=[1 if p in states else 0 for p in pins]
@@ -40,7 +40,9 @@ def all_pins_in():
         pin = Pin(p, Pin.IN, pull=Pin.PULL_UP)
         
 
-def test_pin(pin_test,state=1):
+def test_pin(pin_test,state):
+    print("test_pin",pin_test,state)
+    global total_errors
     lowhigh=['low','high']
     states=[]
     vals={}
@@ -75,72 +77,63 @@ def test_pin(pin_test,state=1):
     if pin_error or pins_short>0:
         print()
         print_pins(states)
-        
-    return pin_error,pins_short
+    total_errors+=pin_error+pins_short
+    print("errors=",total_errors)
+   
 
+def print_start():
+    print("\n\n*********************")
+    print("* Ready for testing *")
+    print("* Insert DUT in rig *")
+    print("*********************")
 
+def start():
+    global total_errors
+    print("[*] Starting test")
+    total_errors=0
+    all_pins_in()
+    np[0]=(0,0,0)
+    np.write()
+    _=Pin(pin_led,Pin.IN)
+     
+def mac(id):
+    print("[*] Testing ID ",id.decode('ascii'))
 
+    
+def stop():
+    if total_errors==0:
+       print("[*] Test passed succesfully")
+    else:
+       print("\n[!] Test failed with %d error(s)"%total_errors)
 
-# wait for start message from DUT
-while True:
-  print("\n\n*********************")
-  print("* Ready for testing *")
-  print("* Insert DUT in rig *")
-  print("*********************")
-  np=NeoPixel(Pin(pin_led),1)
-  np[0]=(50,50,0)
-  np.write()
-  all_pins_in()
-  start=False
-  while not start:
-      msg=uart.read()
-      #print("msg=",msg)
-      sleep_ms(20)
-      if msg==b'start':
-          start=True
-          np[0]=(0,0,0)
-          np.write()
-          Pin(pin_led,Pin.IN)
-  
-  _=uart.write('ack')
-  #print("start testing")
-  testing=True
-  total_errors=0
-  while testing:
-    r=uart.read()
-    if r:
-        r=r.decode('ascii')
-        #print("rcv",r)
-        if len(r)>=1:
-            if r=='stop':
-                testing=False
-            if r[0]=='H' or r[0]=='L':
-                state=1 if r[0]=='H' else 0
-                pin=int(r[1:])
-                pin_error,pin_short=test_pin(pin,state=state)
-                total_errors+=pin_error+pin_short
-                _=uart.write('ack')
-            if r[0]=='I':
-                print("[*] Testing ID %s"%r.split('=')[1])
-                _=uart.write('ack')
-  if total_errors==0:
-     print("[*] Test passed succesfully")
-  else:
-     print("\n[!] Test failed with %d error(s)"%total_errors)
-  np=NeoPixel(Pin(pin_led),1)
-  if total_errors>0:
-      for i in range(5):
+    if total_errors>0:
+       for i in range(5):
           np[0]=(100,0,0)
           np.write()
           sleep_ms(200)
           np[0]=(0,0,0)
           np.write()
           sleep_ms(200)          
-  else:
-      np[0]=(0,100,0)
-      np.write()
-      sleep_ms(1000)
-  np[0]=(0,0,0)
-  np.write()
-  _=Pin(pin_led,Pin.IN)
+    else:
+        np[0]=(0,100,0)
+        np.write()
+        sleep_ms(1000)
+    print_start()
+  
+# initialization
+
+np=NeoPixel(Pin(pin_led),1)
+np[0]=(50,50,0)
+np.write()
+all_pins_in()
+print_start()
+
+
+ur.add_command(start)
+ur.add_command(test_pin)
+ur.add_command(mac)
+ur.add_command(stop)
+
+
+ur.loop()
 
